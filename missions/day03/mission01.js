1db6d85e2266c47f52dc4b653fc8fc8d671f9d0c
hyeoniiee <starjea0126@gmail.com>
2024-09-11 22:43:31 +0900

    day03 - 오류수정

--- missions/day03/mission01.js
+++ missions/day03/mission01.js
@@ -43,5 +43,5 @@ function sumNumbers(a, b) {
   return a + addFive(b);
 }
 
-console.log(sumNumbers(1, 2));
-console.log(sumNumbers(3, 4));
+console.log(sumNumbers(3, 7)); // 출력: 15 (7에 5를 더한 후 3을 더함)
+console.log(sumNumbers(10, 5)); // 출력: 20 (5에 5를 더한 후 10을 더함)
